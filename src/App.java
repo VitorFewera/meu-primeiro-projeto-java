e9a2bc884f02e673cd56cff0a4bae2d162b6c309
vitorferreira <vitorh.ferreira00@gmail.com>
2023-09-30 11:33:58 -0300

    atualização

---
+++ src/App.java
@@ -0,0 +1,26 @@
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        double saldo = 25.0;
+        double valorSolcitado = 17.0;
+
+        if(valorSolcitado < saldo){
+            saldo = saldo - valorSolcitado;
+            System.out.println(saldo);
+        }
+        else{
+            System.out.println("erro");
+        }
+
+        //ternario
+
+        double nota = 8;
+
+        String resultado = nota > 7 || nota == 7 ? "Aprovado" : "Reprovado";
+        System.out.println(resultado);
+
+
+        
+
+    }
+}
